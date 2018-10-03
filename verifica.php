32e845a48f98e8bf625aef986b3eb8e8f23523ba
Jorge Junio <jorge_junio_silva@hotmail.com>
2018-10-03 19:45:57 -0300

    sessao em desenvolvimento

--- verifica.php
+++ verifica.php
@@ -17,29 +17,23 @@ $senha = isset($_GET["senha"]) ?$_GET["senha"] : "";
 */
 $consulta = "SELECT * FROM Cliente WHERE login = '$login' and senha = '$senha' "; 
 $con = $dao->query($consulta) or die("Erro no banco de dados!"); 
-//$total = $dao->num_rows($con); 
- 
- //$consulta = "SELECT * FROM Cliente WHERE login = '$login' and senha = '$senha'"; 
-//$con = $dao->query($consulta) or die($dao->error);
-//$total = $dao->num_rows($con);
-
-
-// Caso o usuário tenha digitado um login válido o número de linhas será 1.. 
-//if($total) 
-// Obtém os dados do usuário, para poder verificar a senha e passar os demais dados para a sessão 
 
 $dados = $con->fetch_array(); 
- 
-// TUDO OK! Agora, passa os dados para a sessão e redireciona o usuário 
-$_SESSION["permissao"] = $dados["tipo"];
-$_SESSION["id_usuario"]= $dados["cpf"]; 
-$_SESSION["nome_usuario"] = $dados["nome"];  
 
+if ($dados != NULL) {
+	$_SESSION["permissao"] = $dados["tipo"];
+	$_SESSION["id_usuario"]= $dados["cpf"]; 
+	$_SESSION["nome_usuario"] = $dados["nome"];
 
-echo $_SESSION["permissao"];
-echo $_SESSION["id_usuario"];
-echo $_SESSION["nome_usuario"]; 
+	echo $_SESSION["permissao"];
+	echo $_SESSION["id_usuario"];
+	echo $_SESSION["nome_usuario"];
 
-//chama a página de listar_fun.php
-exit('<script>location.href = "listar_fun.php"</script>');
+	exit('<script>location.href = "listar_fun.php"</script>');
+	
+}else{
+	exit('<script>location.href = "index.html"</script>');
+}
+
+session_destroy();
 ?>
