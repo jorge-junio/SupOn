6bb0b841abcb71fd67ed0687311eee320ea4b6fb
Jorge Junio <jorge_junio_silva@hotmail.com>
2018-11-01 14:32:17 -0300

    manutencao do codigo

--- verifica.php
+++ verifica.php
@@ -11,29 +11,22 @@
                 min-height: 100%;
                 background-image: linear-gradient(to bottom, rgba(25,25,25, 0.85), rgba(220,220,220, 0.85));
             }
-
             .forms {
                 margin-top: 10%;
             }
         </style>
     </head>
     <body>
-
-
-
         <!--Import jQuery before materialize.js-->
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
-
     </body>
 </html>
 
 <?php 
 	// Conexão com o banco de dados 
 	include("DAO/conexao.php");
-	 
-	// Inicia sessões 
-	session_start(); 
+	
 	 
 	// Recupera o login 
 	$login = isset($_GET["login"]) ?$_GET["login"] : ""; 
@@ -53,6 +46,8 @@
 	$dados = $con->fetch_array(); 
 
 	if ($dados != NULL) {
+		// Inicia sessões 
+		session_start(); 
 		$_SESSION["permissao"] = $dados["tipo"];
 		$_SESSION["id_usuario"]= $dados["cpf"]; 
 		$_SESSION["nome_usuario"] = $dados["nome"];
@@ -60,7 +55,22 @@
 		exit('<script>location.href = "view/adm/listar_fun.php"</script>');
 		
 	}else{
-		exit('<script>location.href = "index.html"</script>');
+		$consulta = "SELECT * FROM Supermercado WHERE login = '$login' and senha = '$senha' "; 
+		$con = $conexao->conecta()->query($consulta) or die("Erro no banco de dados!"); 
+
+		$dados = $con->fetch_array(); 
+
+		if ($dados != NULL) {
+			session_start(); 
+			$_SESSION["permissao"] = "sup";
+			$_SESSION["id_usuario"]= $dados["cnpj"]; 
+			$_SESSION["nome_usuario"] = $dados["nomeF"];
+
+			exit('<script>location.href = "view/adm/listar_fun.php"</script>');
+			
+		}else{
+			exit('<script>location.href = "index.html"</script>');
+		}
 	}
 
 	session_destroy();
--- view/adm/editar_pro.php
+++ view/adm/editar_pro.php
@@ -9,19 +9,42 @@
         ?>
 
         <?php
-        include "../../conexao.php";
+            include "../../conexao.php";
 
-        $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
-        $marca = isset($_GET["marca"]) ? $_GET["marca"] : "";
-        $descricao = isset($_GET["descricao"]) ? $_GET["descricao"] : "";
-        $preco = (FLOAT) isset($_GET["valor"]) ? $_GET["valor"] : 0;
+            $codigo = isset($_GET["codigo"]) ? $_GET["codigo"] : "";
+            $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
+            $marca = isset($_GET["marca"]) ? $_GET["marca"] : "";
+            $descricao = isset($_GET["descricao"]) ? $_GET["descricao"] : "";
+            $preco = (FLOAT) isset($_GET["valor"]) ? $_GET["valor"] : 0;
 
-        $consulta = "UPDATE Produto SET nome = '$nome', marca = '$marca', descricao = '$descricao', 
-            preco = '$preco' WHERE nome = '$nome' ";
+            $consulta = "UPDATE Produto SET nome = '$nome', marca = '$marca', descricao = '$descricao', 
+                preco = '$preco' WHERE codigo = '$codigo' ";
 
-        if ($nome != " ") {
-            $con = $dao->query($consulta) or die($dao->error);
-        }
+            if ($codigo != 0) {
+                $con = $dao->query($consulta) or die($dao->error);
+                exit('<script>location.href = "listar_pro.php"</script>');
+            }
+
+            //pega o codigo do produto que passei via post pelo botão da tabela
+            $codigo = filter_input(INPUT_POST, "codigo", FILTER_SANITIZE_STRING);
+
+            //cria a consulta para pegar os dados do Produto que tem esse codigo que peguei no post
+            $consulta = "SELECT * FROM Produto WHERE codigo = '$codigo' ";
+
+            //executa a query
+            $result = mysqli_query($dao, $consulta);
+
+            //verifica se foi encontrada algum Produto no banco que tenha esse codigo e pega seus dados e joga em variáveis
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) { 
+                    $codigo = $row["codigo"];
+                    $nome = $row["nome"];
+                    $marca = $row["marca"];
+                    $descricao = $row["descricao"];
+                    $preco = (FLOAT) $row["preco"];
+                }
+
+            }
         ?>
     </head>
     <body>
@@ -46,28 +69,35 @@
 
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="nome" type="text" class="validate" name="nome" required="" />
+                            <input id="codigo" type="text" class="validate" name="codigo" required="" value="<?php echo $codigo; ?>" />
+                            <label><i class="material-icons left">shopping_basket</i>Código</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="nome" type="text" class="validate" name="nome" required="" value="<?php echo $nome; ?>" />
                             <label><i class="material-icons left">shopping_basket</i>Nome</label>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="marca" type="text" class="validate" name="marca" required="" />
+                            <input id="marca" type="text" class="validate" name="marca" required="" value="<?php echo $marca; ?>" />
                             <label><i class="material-icons left">receipt</i>Marca</label>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="descricao" type="text" class="validate" name="descricao" required />
+                            <input id="descricao" type="text" class="validate" name="descricao" required="" value="<?php echo $descricao; ?>" />
                             <label><i class="material-icons left">insert_comment</i>Descrição</label>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="valor" type="text" class="validate" name="valor" required />
+                            <input id="valor" type="text" class="validate" name="valor" required="" value="<?php echo $preco; ?>" />
                             <label><i class="material-icons left">monetization_on</i>Valor</label>
                         </div>
                     </div>
