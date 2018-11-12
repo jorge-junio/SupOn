ff8a42990c43ba3982eef29456f242711757d75d
ThalisonCaio <thalison.caio@gmail.com>
2018-11-12 11:20:21 -0300

    layout dos mod cliente e supermercado

---
+++ view/cli_carrinho.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php
+        //valida a sessão
+        include "../valida.php";
+
+        //imports
+        include "includes/headTop.html";
+        include "../DAO/conexao.php";
+        include "../controller/ProdutoController.php";
+        ?>
+    </head>
+    <body>
+
+        <!-- Navbar goes here -->
+        <?php
+        include "./includes/menuCliente.html";
+        ?>
+
+        <div class="container">
+            <div class="section"></div>
+            <div class="section"></div>
+            <div class="row white darken-4 ">
+                <div class="container">
+
+                    <?php
+                    ?>
+
+                </div>
+                <div class="section"></div>
+            </div>
+        </div>
+
+        <?php
+        include "includes/scriptFim.html";
+        ?>
+
+    </body>
+</html>
