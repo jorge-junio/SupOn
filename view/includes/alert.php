2ade1b695586383bf1f6ea5e5b84b472d24b94ad
ThalisonCaio <thalison.caio@gmail.com>
2018-11-15 15:41:03 -0300

    implementação do alert

--- view/includes/alert.php
+++ view/includes/alert.php
@@ -1,14 +1,14 @@
 <div class="row" id="alert_box">
-  <div class="col s12 m12">
+  <div class="col s12">
     <div class="card <?php echo $_SESSION['type']; ?> darken-1">
       <div class="row">
-        <div class="col s12 m10">
+        <div class="col s12">
           <div class="card-content white-text">
             <?php echo $_SESSION['message']; ?>
         </div>
       </div>
-      <div class="col s12 m2">
-        <i class="fa fa-times icon_style"                             id="alert_close" aria-hidden="true"></i>
+      <div class="col s12">
+        <i class="material-icons icon_style" id="alert_close" aria-hidden="true">close</i>
       </div>
     </div>
    </div>
