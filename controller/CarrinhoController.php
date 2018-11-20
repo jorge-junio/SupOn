b0ed54f87bc6bb49a544b767cd5a135c613e3236
narcisof <36757413+narcisof@users.noreply.github.com>
2018-11-20 02:40:43 -0800

    Dev Carrinho Controller

---
+++ controller/CarrinhoController.php
@@ -0,0 +1,102 @@
+<?php
+
+include_once '../DAO/carrinhoDAO.php';
+include_once '../DAO/conexao.php';
+include_once '../model/carrinho.php';
+
+class ProdutoController {
+    //codigo, $data, $cpfCliente, $produtos
+    public function insereCarrinho() {
+    
+        $codigo = filter_input(INPUT_POST,"codigo",FILTER_SANITIZE_STRING);
+        $data = filter_input(INPUT_POST,"data",FILTER_SANITIZE_STRING);
+        $cpfCliente = filter_input(INPUT_POST,"cpfCliente",FILTER_SANITIZE_STRING);
+        $produtos = filter_input(INPUT_POST,"´produtos",FILTER_SANITIZE_STRING);
+        
+        $conexao = new conexao();
+        $carrinho = new carrinho();
+        $carrinho->setCodigo($codigo);
+        $carrinho->setData($data);
+        $carrinho->setCpfCliente($cpfCliente);
+        $carrinho->setProdutos($produtos);
+        $carrinhoDAO = new carrinhoDAO();
+        $carrinhoDAO->adicionar($conexao, $carrinho);
+    }
+  
+    public function listaCarrinho() {
+        $conexao = new conexao();
+        $carrinhoDAO = new carrinhoDAO();
+        $carrinhoDAO->listar($conexao);
+    }
+ 
+    public function excluiCarrinho() {
+            $codigo = $_POST['codigo'];
+            $conexao = new conexao();
+            $carrinho = new carrinho();
+            $carrinho->setCodigo($codigo);
+            $carrinhoDAO = new carrinhoDAO();
+            $carrinhooDAO->remover($conexao, $carrinho);
+        
+    }
+    
+    public function editaCarrinho() {
+        $codigo = filter_input(INPUT_POST,"codigo",FILTER_SANITIZE_STRING);
+        $data = filter_input(INPUT_POST,"data",FILTER_SANITIZE_STRING);
+        $cpfCliente = filter_input(INPUT_POST,"cpfCliente",FILTER_SANITIZE_STRING);
+        $produtos = filter_input(INPUT_POST,"´produtos",FILTER_SANITIZE_STRING);
+                
+        $conexao = new conexao();
+        $carrinho = new carrinho();
+        $carrinho->setCodigo($codigo);
+        $carrinho->setData($data);
+        $carrinho->setCpfCliente($cpfCliente);
+        $carrinho->setProdutos($produtos);
+        $carrinhoDAO = new carrinhoDAO();
+        $carrinhoDAO->alterar($conexao, $carrinho);
+        
+
+    }
+
+    public function selecionaCarrinho($codigo) {
+
+        $conexao = new conexao();
+        $carrinho = new carrinho();
+        $carrinho->setCodigo($codigo);
+        $carrinhoDAO = new produtoDAO();
+        return $carrinhoDAO->selecionar($conexao, $carrinho);
+         
+    }
+
+    public function listaBusca($codCarrinho) {
+        $conexao = new conexao();
+        $carrinhoDAO = new carrinhoDAO();
+        $carrinhoDAO->listarBusca($conexao, $codCarrinho);
+    }
+}
+
+$carrinho = new CarrinhoController();
+
+// se apertou casdastar, $cadastrar recebe $_POST['cadastrar(name do input)']...
+        $codigo = filter_input(INPUT_POST,"codigo",FILTER_SANITIZE_STRING);
+        $data = filter_input(INPUT_POST,"data",FILTER_SANITIZE_STRING);
+        $cpfCliente = filter_input(INPUT_POST,"cpfCliente",FILTER_SANITIZE_STRING);
+        $produtos = filter_input(INPUT_POST,"´produtos",FILTER_SANITIZE_STRING);
+
+if (isset($cadastrar)) {
+    $carrinho->insereCarrinho();
+    header("Location: ../view/listar_carrinho.php");
+}
+
+if (isset($excluir)) {
+    $carrinho->excluiCarrinho();
+    header("Location: ../view/listar_carrinho.php");
+}
+
+if (isset($editar)) {
+    $carrinho->editaCarrinho();
+    header("Location: ../view/listar_carrinho.php");
+}
+
+if (isset($direcionaListar)) {
+    header("Location: ../view/listar_carrinho.php");
+}
