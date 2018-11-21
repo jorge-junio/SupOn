87ced4edf612c7aa9765a6e2b937dc9e0f7ece6f
Jorge Junio <jorge_junio_silva@hotmail.com>
2018-11-21 16:18:51 -0300

    implementacao mask

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $("#Cpf").mask("000.000.000-00");
+});
+
+/*
+$(function(){
+    var input = $('#[SEU_ID]');
+    input.on('focusin', function() {
+        input.mask('[TAMANHO_MASCARA]', {reverse: true});
+    });
+    input.on('focusout', function() {
+        input.mask('[SUA_MASCARA_FORMATADA]', {reverse: true});
+    });
+});
+*/
